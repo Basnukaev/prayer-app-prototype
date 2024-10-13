aeb281ff531baa38445cb3a320dd1a67079713ed
Abdula Basnukaev <basnukaev.abdula@gmail.com>
2024-10-13 17:45:41 +0300

    New welcome page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -32,42 +32,40 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Ввод пропущенных намазов'),
+        backgroundColor: const Color(0xFF73C8A9),
+        title: const Text(
+          'Ввод пропущенных намазов',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _fajrController,
-              decoration: const InputDecoration(labelText: 'Фаджр'),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              controller: _zuhrController,
-              decoration: const InputDecoration(labelText: 'Зухр'),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              controller: _asrController,
-              decoration: const InputDecoration(labelText: 'Аср'),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              controller: _maghribController,
-              decoration: const InputDecoration(labelText: 'Магриб'),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              controller: _ishaController,
-              decoration: const InputDecoration(labelText: 'Иша'),
-              keyboardType: TextInputType.number,
-            ),
-            TextField(
-              controller: _allController,
-              decoration: const InputDecoration(labelText: 'Каждый намаз'),
-              keyboardType: TextInputType.number,
-              onChanged: (value) {
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF73C8A9), // светлый зеленый
+              Color(0xFF373B44), // темный серо-синий
+            ],
+          ),
+        ),
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              _buildPrayerInputField('Фаджр', _fajrController),
+              _buildPrayerInputField('Зухр', _zuhrController),
+              _buildPrayerInputField('Аср', _asrController),
+              _buildPrayerInputField('Магриб', _maghribController),
+              _buildPrayerInputField('Иша', _ishaController),
+              _buildPrayerInputField('Каждый намаз', _allController, onChanged: (value) {
                 setState(() {
                   _fajrController.text = value;
                   _zuhrController.text = value;
@@ -75,42 +73,98 @@ class _InputPageState extends State<InputPage> {
                   _maghribController.text = value;
                   _ishaController.text = value;
                 });
-              },
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                final result = await Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CalculatorPage()),
-                );
-                if (result != null) {
-                  setState(() {
-                    _allController.text = result.toString();
-                    _fajrController.text = result.toString();
-                    _zuhrController.text = result.toString();
-                    _asrController.text = result.toString();
-                    _maghribController.text = result.toString();
-                    _ishaController.text = result.toString();
-                  });
-                }
-              },
-              child: const Text('Рассчитать пропущенные намазы'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                await _savePreferences();
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const TrackingPage(),
-                  ),
-                );
-              },
-              child: const Text('Продолжить'),
-            ),
-          ],
+              }),
+              const SizedBox(height: 20),
+              _buildActionButton(
+                text: 'Рассчитать пропущенные намазы',
+                onPressed: () async {
+                  final result = await Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CalculatorPage()),
+                  );
+                  if (result != null) {
+                    setState(() {
+                      _allController.text = result.toString();
+                      _fajrController.text = result.toString();
+                      _zuhrController.text = result.toString();
+                      _asrController.text = result.toString();
+                      _maghribController.text = result.toString();
+                      _ishaController.text = result.toString();
+                    });
+                  }
+                },
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              ),
+              const SizedBox(height: 20),
+              _buildActionButton(
+                text: 'Продолжить',
+                onPressed: () async {
+                  await _savePreferences();
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const TrackingPage()),
+                  );
+                },
+              ),
+              const SizedBox(height: 10), // небольшой отступ снизу
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Метод для создания полей ввода
+  Widget _buildPrayerInputField(String label, TextEditingController controller,
+      {ValueChanged<String>? onChanged}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: label,
+          labelStyle: const TextStyle(
+            color: Colors.white30,
+          ),
+          // светло-зеленый для label
+          filled: true,
+          fillColor: Colors.white.withOpacity(0.8),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Color(0xFF73C8A9), width: 2),
+          ),
+        ),
+        keyboardType: TextInputType.number,
+        onChanged: onChanged,
+      ),
+    );
+  }
+
+  // Метод для создания кнопок
+  Widget _buildActionButton({
+    required String text,
+    required VoidCallback onPressed,
+    EdgeInsetsGeometry padding = const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+  }) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: padding,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        backgroundColor: const Color(0xFFF4A261), // Светло-оранжевый цвет для кнопки
+      ),
+      onPressed: onPressed,
+      child: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: const TextStyle(
+          fontSize: 18,
+          color: Colors.white,
         ),
       ),
     );
--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -6,34 +6,71 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Добро пожаловать!',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Желаем вам удачи и успехов в восполнении намазов.',
-                style: TextStyle(fontSize: 18),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/input');
-                },
-                child: const Text('Далее'),
-              ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF73C8A9), // светлый зеленый оттенок
+              Color(0xFF373B44), // темный серо-синий оттенок
             ],
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'إنَّ أوَّلَ ما يُحاسَبُ بِهِ العبدُ يومَ القيامةِ من عملِه صلاتُهُ',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                    fontFamily: 'Simplified Arabic', // Проверь, добавлен ли шрифт
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Первое, за что человек будет спрошен в День суда из своих дел — это молитва.',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white70,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 40),
+                const Text(
+                  'Добро пожаловать!',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    backgroundColor: Color(0xFFF4A261), // Светло-оранжевый цвет для кнопки
+                  ),
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/input');
+                  },
+                  child: const Text(
+                    'Начнем',
+                    style: TextStyle(fontSize: 18, color: Colors.white), // Белый текст
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
